ea8d25bf21a5c9033e178c08f95b446752dd0515
wavesinaroom <juan@wavesinaroom.com>
2023-01-30 15:28:31 -0500

    [Feature] Gameboard tests

--- tdd.test.js
+++ tdd.test.js
@@ -1,22 +1,56 @@
 import {shipTypes, Ship} from './ship'
+import Gameboard from './gameboard.js'
 
-describe('Ship',()=>{
+describe(`Ship`,()=>{
   const myShip = Ship(shipTypes.BATTLESHIP) 
-  test('Create a Battleship', ()=>{
+  test(`Create a Battleship`, ()=>{
     expect(myShip.length).toBe(4);
   })
-  test('Throw exception if no shipType is specified',()=>{
+  test(`Throw exception if no shipType is specified`,()=>{
     expect(()=>{Ship()}).toThrowError(`Can't create ship without a specific ship type`);
   })
-  test('Hit points increase when hit function is called', ()=>{
+  test(`Hit points increase when hit function is called`, ()=>{
     myShip.hit();
     expect(myShip.hitpoints).toBe(1);
   })
-  test('Ship is sunk', ()=>{
+  test(`Ship is sunk`, ()=>{
     myShip.hit();
     myShip.hit();
     myShip.hit();
     myShip.hit();
     expect(myShip.isSunk).toBeTruthy();
   })
+})
+
+describe(`Gameboard`,()=>{
+  const myBoard = Gameboard();
+  test(`Create a 10x10 Gameboard`, ()=>{
+    expect(myBoard.size).toBe(10); 
+  })
+  test(`Place a horizontal BATTLESHIP on the board center`, ()=>{
+    expect(myBoard.cells[4][4]).toBe("B1");
+    expect(myBoard.cells[5][4]).toBe("B1");
+    expect(myBoard.cells[6][4]).toBe("B1");
+    expect(myBoard.cells[7][4]).toBe("B1");
+  })
+  test(`Place a vertical BATTLESHIP on the board center`, ()=>{
+    expect(myBoard.cells[4][4]).toBe("B1");
+    expect(myBoard.cells[4][5]).toBe("B1");
+    expect(myBoard.cells[4][6]).toBe("B1");
+    expect(myBoard.cells[4][7]).toBe("B1");
+  })
+  test(`A ship can't never be placed out of boundaries`,()=>{
+    expect(()=>{myBoard.placeShips()}).toThrowError(`Ship is out of boundaries`);
+  })
+  test(`First BATTLESHIP ID is stored in board records`,()=>{
+    expect(myBoard.ships[0]).toBe("B1", [4][4], shipOrientation.HORIZONTAL);
+  })
+  test(`Board records missed shots`, ()=>{
+    expect(myBoard.missedShots[0]).toBe([3][3]);
+  })
+  test(`Receives effective attack coordinates to increase ship on board hitpoints`, ()=>{
+    expect(myBoard.getAttack).toHaveBeenCalled();
+    expect(myBoard.ships[0].hit()).toHaveBeenCalled();
+    expect(myBoard.ships[0].hitpoints).toBe(1);
+  })    
 })
