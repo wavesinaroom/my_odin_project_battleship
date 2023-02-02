fbad1827b63f4bc533653399269e319768ba1515
wavesinaroom <juan@wavesinaroom.com>
2023-02-02 12:47:35 -0500

    [Task] Store ship/shots positions as tile objects that take coordinates as strings and ship.id string/undefined as object in a array for better handling

--- gameboard.js
+++ gameboard.js
@@ -10,44 +10,57 @@ function coordinate(x,y){
   return{x:x, y:y}
 }
 
+function tile(coordinate, id){
+  return{coordinate: coordinate, id: id}
+}
+
 const shipOrientation = {
   HORIZONTAL: Symbol(`horizontal`),
   VERTICAL: Symbol(`vertical`),
 }
 
 const gameboardActions = {
-  placeShips(shipType, orientation, coordinate){
+  placeShips(shipType, orientation, inputCoordinate){
     const ship = Ship(shipType);
-    ship.ID = `${coordinate.x},${coordinate.y}`;
-    let newCoordinate; 
+    ship.ID = `${inputCoordinate.x},${inputCoordinate.y}`;
   
-    if(this.tiles.has(`${coordinate.x},${coordinate.y}`))
-      throw new Error(`There's already an object on that coordinate`);
+    this.tiles.forEach(tile=>{
+      if(tile.coordinate.x === inputCoordinate.x && tile.coordinate.y === inputCoordinate.y)
+      throw new Error(`There's already an object on that inputCoordinate`);
+    })
 
     if(orientation === shipOrientation.HORIZONTAL){
-      if(coordinate.x + ship.length > this.size)
+      if(inputCoordinate.x + ship.length > this.size)
         throw new Error(`Part of ship is out of board X boundary`);
-      for(let i = 0; i<ship.length; ++i){
-        newCoordinate = `${coordinate.x+i},${coordinate.y}`
-        this.tiles.set(newCoordinate, ship.ID);
-      }
+      for(let i = 0; i<ship.length; ++i)
+        this.tiles.push(tile(`${inputCoordinate.x+i},${inputCoordinate.y}`), `${ship.ID}`);
     }else if(orientation === shipOrientation.VERTICAL){
-      if(coordinate.y + ship.length > this.size)
+      if(inputCoordinate.y + ship.length > this.size)
         throw new Error(`Part of ship is out of board Y boundary`);
-      for(let i = 0; i<ship.length; ++i){
-        newCoordinate = `${coordinate.x},${coordinate.y+i}`
-        this.tiles.set(newCoordinate, ship.ID);
-      }
+      for(let i = 0; i<ship.length; ++i)
+        this.tiles.push(tile(`${inputCoordinate.x},${inputCoordinate.y+i}`), `${ship.ID}`);
     }else
       throw new Error(`Undefined ship orientation`);
 
-    this.shipsLog.set(ship.ID, ship);
+    this.shipsLog.set(`${ship.ID}`,ship);
   },
-  getAttack(coordinate){
-    if(!this.tiles.has(`${coordinate.x},${coordinate.y}`))
-      this.tiles.set(`${coordinate.x},${coordinate.y}`, undefined);
-    else{
-      this.shipsLog.get(this.tiles.get(`${coordinate.x},${coordinate.y}`)).hit();
+
+  getAttack(inputCoordinate){
+    let isShip, ID;
+    this.tiles.forEach(tile=>{
+      if(tile.coordinate.y===inputCoordinate.y&&tile.coordinate.x===inputCoordinate.x)
+        if(tile.id){
+          ID = tile.id;
+          return isShip = true;
+        }
+    })
+    isShip?this.shipsLog.get(ID).hit():this.shipsLog.set(`${inputCoordinate.x},${inputCoordinate.y}`, undefined);
+  },
+
+  removeShip(shipID){
+    const it = this.tiles.values();
+    for(let i = 0; i>this.tiles.size; ++i){
+      
     }
   },
 }
@@ -55,8 +68,8 @@ const gameboardActions = {
 
 function GameBoard (){
   const gameboard = Object.create(gameboardActions);
-  gameboard.shipsLog= new Map(); 
-  gameboard.tiles= new Map(); 
+  gameboard.shipsLog = new Map();
+  gameboard.tiles= []; 
   gameboard.size= 10;
 
     return gameboard;
--- gameboard.test.js
+++ gameboard.test.js
@@ -1,58 +1,61 @@
-import {shipType,Ship} from './ship'
-import { GameBoard, coordinate, shipOrientation } from "./gameboard";
-const myBoard = GameBoard();
+  import {shipType,Ship} from './ship'
+  import { GameBoard, coordinate, shipOrientation } from "./gameboard";
+  const myBoard = GameBoard();
 
-describe(`Game board set up`, ()=>{
-  test('Game board size has to be 10',()=>{
-    expect(myBoard.size).toBe(10);
+  describe(`Game board set up`, ()=>{
+    test('Game board size has to be 10',()=>{
+      expect(myBoard.size).toBe(10);
+    });
   });
-});
-describe(`Ship placement`,()=>{
-  let myCoordinate = coordinate(4,4);
-  test(`Place a DESTROYER ship on board center`, ()=>{
-    myBoard.placeShips(shipType.DESTROYER, shipOrientation.HORIZONTAL, myCoordinate);  
-    expect(myBoard.tiles.has(`${myCoordinate.x},${myCoordinate.y}`)).toBeTruthy();
-  })
-  test(`Ships can't overlap on board`,()=>{
-    expect(()=>{myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.VERTICAL, myCoordinate)}).toThrowError(`There's already an object on that coordinate`);
-  })
-  test(`Place a horizontal BATTLESHIP on board`, ()=>{
-    myCoordinate.x = 5;
-    myCoordinate.y = 5;
-    myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.HORIZONTAL, myCoordinate);
-    expect(myBoard.tiles.has(`${myCoordinate.x+3},${myCoordinate.y}`)).toBeTruthy();
-  })
-  test(`Place a vertical BATTLESHIP on board`, ()=>{
-    myCoordinate.y = 1;
-    myCoordinate.x = 1;
-    myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.VERTICAL, myCoordinate);
-    expect(myBoard.tiles.has(`${myCoordinate.x},${myCoordinate.y+3}`)).toBeTruthy();
+  describe(`Ship placement`,()=>{
+    let myCoordinate = coordinate(4,4);
+    test(`Place a DESTROYER ship on board center`, ()=>{
+      myBoard.placeShips(shipType.DESTROYER, shipOrientation.HORIZONTAL, myCoordinate);  
+      expect(myBoard.tiles.has(`${myCoordinate.x},${myCoordinate.y}`)).toBeTruthy();
+    })
+    test(`Ships can't overlap on board`,()=>{
+      expect(()=>{myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.VERTICAL, myCoordinate)}).toThrowError(`There's already an object on that coordinate`);
+    })
+    test(`Place a horizontal BATTLESHIP on board`, ()=>{
+      myCoordinate.x = 5;
+      myCoordinate.y = 5;
+      myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.HORIZONTAL, myCoordinate);
+      expect(myBoard.tiles.has(`${myCoordinate.x+3},${myCoordinate.y}`)).toBeTruthy();
+    })
+    test(`Place a vertical BATTLESHIP on board`, ()=>{
+      myCoordinate.y = 1;
+      myCoordinate.x = 1;
+      myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.VERTICAL, myCoordinate);
+      expect(myBoard.tiles.has(`${myCoordinate.x},${myCoordinate.y+3}`)).toBeTruthy();
+    })
+
+    test(`Part of a ship is out of X boundaries`,()=>{
+      myCoordinate.x = 7;
+      myCoordinate.y = 7;
+      expect(()=>{myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.HORIZONTAL, myCoordinate)}).toThrowError(`Part of ship is out of board X boundary`);
+      
+    })
+    test(`Part of a ship is out of Y boundaries`,()=>{
+      myCoordinate.x = 6;
+      expect(()=>{myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.VERTICAL, myCoordinate)}).toThrowError(`Part of ship is out of board Y boundary`);
+    })
   })
 
-  test(`Part of a ship is out of X boundaries`,()=>{
-    myCoordinate.x = 7;
-    myCoordinate.y = 7;
-    expect(()=>{myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.HORIZONTAL, myCoordinate)}).toThrowError(`Part of ship is out of board X boundary`);
-    
+  describe(`Shots`,()=>{
+    let myCoordinate=coordinate(8,8);
+    test(`Failed shot logs undefined in GameBoard tiles`, ()=>{
+      myBoard.getAttack(myCoordinate);
+      expect(myBoard.tiles.get(`${myCoordinate.x},${myCoordinate.y}`)).toBe(undefined);
+    })
+    test(`Hits a ship`,()=>{
+      myCoordinate = coordinate(4,4);
+      myBoard.getAttack(myCoordinate);
+      expect(()=>{myBoard.shipsLog.get(myBoard.tiles.get(`${coordinate.x},${coordinate.y}`))}).not.toBe(undefined);
+    })
+    test(`Sinking ship`, ()=>{
+       
+    })
   })
-  test(`Part of a ship is out of Y boundaries`,()=>{
-    myCoordinate.x = 6;
-    expect(()=>{myBoard.placeShips(shipType.BATTLESHIP,shipOrientation.VERTICAL, myCoordinate)}).toThrowError(`Part of ship is out of board Y boundary`);
-  })
-})
-
-describe(`Shots`,()=>{
-  let myCoordinate=coordinate(8,8);
-  test(`Failed shot logs undefined in GameBoard tiles`, ()=>{
-    myBoard.getAttack(myCoordinate);
-    expect(myBoard.tiles.get(`${myCoordinate.x},${myCoordinate.y}`)).toBe(undefined);
-  })
-  test(`Hits a ship`,()=>{
-    myCoordinate = coordinate(4,4);
-    myBoard.getAttack(myCoordinate);
-    expect(()=>{myBoard.shipsLog.get(myBoard.tiles.get(`${coordinate.x},${coordinate.y}`))}).not.toBe(undefined);
-  })
-})
 
 describe(`Ship log`, ()=>{
   let myCoordinate = coordinate(4,4);
