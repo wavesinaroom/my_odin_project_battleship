54f69ff8057fcb345b91a8fc7ba58d820c46d271
wavesinaroom <juan@wavesinaroom.com>
2023-02-07 16:00:18 -0500

    [Task] Sketch gameOver method inside GameManager

--- gamemanager.js
+++ gamemanager.js
@@ -12,6 +12,19 @@ const GameManager = {
   },
 
   changeTurn(who){
-    who === `CPU`? this.turn = this.player.name : this.turn = who;
+    if(this.player.board.sunkFleet||this.cpu.sunkFleet)
+      gameOver();
+    else
+      who === `CPU`? this.turn = this.player.name : this.turn = who;
+  },
+
+  gameOver(){
+    this.player.board.tiles.splice(0, this.player.tiles.length-1);
+    this.player.board.shipsLog.clear();
+    this.player = undefined;
+
+    this.cpu.board.tiles.splice(0, this.cpu.tiles.length-1);
+    this.cpu.board.shipsLog.clear();
   }
+
 }
