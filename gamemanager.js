035490cb9da5a49c40bcbd6c54f8228a000dd5c2
wavesinaroom <juan@wavesinaroom.com>
2023-02-07 14:45:53 -0500

    [Feature] Player set up method

--- gamemanager.js
+++ gamemanager.js
@@ -5,4 +5,9 @@ const GameManager = {
   turn: undefined,
   player: undefined,
   cpu: Player(),
+  
+  setUpPlayer(name){
+    this.player = Player(name);
+    this.player = this.player.name;
+  }
 }
