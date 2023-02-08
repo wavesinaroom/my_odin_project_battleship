82524a5437c708ba812766006bd5c382148c2c14
wavesinaroom <juan@wavesinaroom.com>
2023-02-08 15:19:48 -0500

    [Feature] Turn change test

--- gamemanager.test.js
+++ gamemanager.test.js
@@ -1,4 +1,8 @@
+import {coordinate} from "./gameboard"
 import {GameManager} from "./gamemanager"
+import {Player} from "./player"
+
+GameManager.cpu = Player();
 
 describe(`Player set up`,()=>{
   GameManager.setUpPlayer(`Amy`)
@@ -11,7 +15,10 @@ describe(`Player set up`,()=>{
 })
 
 describe(`Turn changes`, ()=>{
-
+  const turnSpy = jest.spyOn(GameManager, 'changeTurn')
+  GameManager.player.fire(coordinate(4,4));
+  expect(turnSpy).toBeCalled();
+  expect(GameManager.turn).toStrictEqual(`Amy`);
 })
 
 describe(`End game`, ()=>{
