5b3149f20f23e4890e1b2d681642822bcf36e814
wavesinaroom <juan@wavesinaroom.com>
2023-02-08 11:22:32 -0500

    [Feature] Pablo fires a missile, he misses his target ship though

--- player.test.js
+++ player.test.js
@@ -1,9 +1,10 @@
-import {GameBoard, coordinate} from "./gameboard";
+import {GameBoard, coordinate, shipOrientation} from "./gameboard";
+import { shipType } from "./ship";
 import {GameManager} from './gamemanager';
 import {Player} from "./player";
 import {EventManager} from "./eventmanager";
 
-GameManager.player = Player(`Pablo`);
+GameManager.player = Player(`Pablo`)
 
 describe(`Player set up`, ()=>{
   test(`Player's name is Pablo`,()=>{
@@ -15,13 +16,20 @@ describe(`Player set up`, ()=>{
 })
 
 describe(`Player's actions`,()=>{
-  test(`Pablo fires a missile, he misses ship target though`, ()=>{
-    const myCoordinate = coordinate(5,5);
-    const spy = jest.spyOn(EventManager, 'notifyAttack')
+  GameManager.cpu = Player();
+  GameManager.cpu.board.placeShips(shipType.DESTROYER, shipOrientation.HORIZONTAL, coordinate(4,4))
+  let myCoordinate = coordinate(5,5);
+  const eventManager = jest.spyOn(EventManager, 'notifyAttack');
+  const cpuGetsAttack = jest.spyOn(GameManager.cpu.board, 'getAttack')
+
+  test(`Pablo fires a missile, he misses his target ship  though`, ()=>{
     GameManager.player.fire(myCoordinate);
-    expect(spy).toBeCalled();
-    expect(GameManager.cpu.getAttack).toBeCalledWith(myCoordinate);
-    expect(GameManager.cpu.shipsLog.has(`${myCoordinate.x}, ${myCoordinate.y}`));
+    expect(eventManager).toBeCalled();
+    expect(cpuGetsAttack).toBeCalledWith(myCoordinate);
+    expect(GameManager.cpu.board.shipsLog.has(`${myCoordinate.x},${myCoordinate.y}`)).toBeFalsy();
+  })
+  test(`Pablo fires a missile and hits ship target`, ()=>{
+     
   })
 })
 
