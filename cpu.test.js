3718b74fd2d65ff5ee195c594e2e09ab9e694d88
wavesinaroom <juan@wavesinaroom.com>
2023-02-09 15:44:37 -0500

    [Feature] Fire missile with random coordinates test

--- cpu.test.js
+++ cpu.test.js
@@ -1,3 +1,4 @@
+import {EventManager} from "./eventmanager";
 import {coordinate, tile} from "./gameboard";
 import { GameManager } from "./gamemanager";
 
@@ -10,5 +11,11 @@ describe(`Attack`, ()=>{
   test(`Checks random coordinate is not in tiles list already`, ()=>{
     GameManager.cpu.board.tiles.push(tile(GameManager.cpu.randomCoordinate(), GameManager.cpu.name))
     expect(GameManager.cpu.board.tiles).not.toContainEqual(tile(GameManager.cpu.randomCoordinate(), GameManager.cpu.name));
+  })
+  test(`Fires a missile with random coordinates`, ()=>{
+    const eventSpy = jest.spyOn(EventManager, 'notifyAttack');
+    GameManager.turn = GameManager.cpu.name;
+    GameManager.cpu.fire();
+    expect(eventSpy).toBeCalled();
   })
 })
