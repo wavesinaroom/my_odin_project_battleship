f7e79fdd1b572b0145aca8a955fef42bb5cdfcaf
wavesinaroom <juan@wavesinaroom.com>
2023-02-09 14:59:06 -0500

    [Task] Refactor random coordinate generation test

--- cpu.test.js
+++ cpu.test.js
@@ -4,9 +4,7 @@ import { GameManager } from "./gamemanager";
 GameManager.setUpGame(`Pablo`)
 describe(`Attack`, ()=>{
   test(`Generates a random coordinate`,()=>{
-    const coordinateOne = GameManager.cpu.randomCoordinate();
-    const coordinateTwo = GameManager.cpu.randomCoordinate();
-    expect(coordinateOne).not.toBeUndefined();
-    expect(coordinateOne).not.toEqual(coordinateTwo);
+    expect(()=>{GameManager.cpu.randomCoordinate()}).not.toBeUndefined();
+    expect(()=>{GameManager.cpu.randomCoordinate()}).not.toEqual(()=>{GameManager.cpu.randomCoordinate()});
   })
 })
