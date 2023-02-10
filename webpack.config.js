59ae367fd63f0592bfa73ec797046a8416749520
wavesinaroom <juan@wavesinaroom.com>
2023-02-10 16:20:48 -0500

    [Task] Set up webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   mode: 'development',
   entry:'./src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       tile: 'Battleship'
@@ -14,6 +17,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true, 
+  },
+  optimization:{
+    runtimeChunk: 'single',
   },
   module:{
     rules:[
