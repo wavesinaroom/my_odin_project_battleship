b3e1a8c626cc35772b6f726e600ba4fe318caa07
wavesinaroom <jaureguij@javeriana.edu.co>
2023-02-21 01:43:22 -0500

    [Task] Fire a missile from GameManager.cpu inside eventmanager.test.js

---
+++ src/eventmanager.test.js
@@ -0,0 +1,13 @@
+import {EventManager} from "./eventmanager";
+import { coordinate } from "./gameboard";
+import {GameManager} from "./gamemanager";
+
+GameManager.player = Player(`Pablo`);
+GameManager.cpu = Player(`CPU`);
+
+describe(`Handling a player's attack`,()=>{
+  const attackSpy = jest.spyOn(EventManager, 'handleAttack');
+  GameManager.cpu.fire(coordinate(4,4));
+  expect(attackSpy).toBeCalledWith(`CPU`,coordinate(4,4));
+  expect(EventManager.hit).toBeTruthy();
+});
