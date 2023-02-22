f7c490a812ad53d45f4efb4dfcf3efe9d782bc07
wavesinaroom <jaureguij@javeriana.edu.co>
2023-02-22 00:50:06 -0500

    [Feature] Test cpu and player are actual instances of Player(), not just names

--- src/gamemanager.test.js
+++ src/gamemanager.test.js
@@ -1,5 +1,7 @@
+import {experiments} from "webpack";
 import {EventManager} from "./eventmanager";
 import {GameManager} from "./gamemanager";
+import {Player} from "./player";
 EventManager.player = jest.fn(()=>{
   const name = 'James';
   function fire (){ console.log('here') }
@@ -13,5 +15,12 @@ describe(`Player set up`,()=>{
   test(`GameManager sets up CPU automatically`, ()=>{
     GameManager.setUpGame('Pablo');
     expect(GameManager.cpu.name).toBe(`CPU`);
+  });
+  test(`GameManager turn is set up to Player`,()=>{
+    expect(GameManager.turn).toBe(GameManager.player.name);
+  });
+  test(`CPU and player are actual instances of Player()`,()=>{
+    expect(GameManager.player).toMatchObject(Player(GameManager.player.name));
+    expect(GameManager.cpu).toMatchObject(Player());
   });
 });
