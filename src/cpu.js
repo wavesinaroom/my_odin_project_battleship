7db732e25f42cc4ba56571da29f969ad4addf0b5
wavesinaroom <jaureguij@javeriana.edu.co>
2023-02-23 15:04:19 -0500

    [Feature] randomShipType method and test

--- src/cpu.js
+++ src/cpu.js
@@ -1,7 +1,23 @@
 import {coordinate} from './gameboard'
-export {randomCoordinate};
+import {shipType} from './ship';
+export {randomCoordinate, randomShipType};
 
 function randomCoordinate(){
+  //Come back here if you feel 9 is never throw out
   return coordinate(Math.floor(Math.random()*9),Math.floor(Math.random()*9));
+}
+
+function randomShipType(){
+
+  switch(Math.floor((Math.random()*4))){
+    case 0:
+      return shipType.DESTROYER;
+    case 1:
+      return shipType.CARRIER;
+    case 2:
+      return shipType.CRUISER;
+    case 3:
+      return shipType.BATTLESHIP;
+  }
 }
 
--- src/cpu.test.js
+++ src/cpu.test.js
@@ -1,5 +1,4 @@
-import {randomCoordinate} from './cpu'
-import {coordinate} from './gameboard';
+import {randomCoordinate, randomShipType} from './cpu'
 
 describe(`Random values`,()=>{
   test(`randomCoordinate`,()=>{
@@ -9,5 +8,9 @@ describe(`Random values`,()=>{
     expect(aCoordinate.x).toBeLessThanOrEqual(9);
     expect(aCoordinate.y).toBeGreaterThanOrEqual(0);
     expect(aCoordinate.y).toBeLessThanOrEqual(9);
+  });
+  test(`Random ship type`,()=>{
+    const aShiptType = randomShipType();
+    expect(aShiptType).not.toBeUndefined();
   });
 });
